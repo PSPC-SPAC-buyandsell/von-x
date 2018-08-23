60752eaa269b7d1231d2ba696d9b4075242cc5e1
Andrew Whitehead <andrew@1crm.com>
2018-08-23 14:28:13 -0700

    updates to header verification API

--- vonx/web/headers.py
+++ vonx/web/headers.py
@@ -15,9 +15,14 @@
 # limitations under the License.
 #
 
-import logging
+"""
+Abstractions to remove direct client dependency on didauth library
+"""
 
-from didauth.base import KeyFinderBase
+import logging
+from typing import Mapping
+
+from didauth.base import KeyFinderBase, StaticKeyFinder
 from didauth.error import VerifierException
 from didauth.headers import HeaderVerifier
 from didauth.utils import decode_string
@@ -28,7 +33,17 @@ from vonx.indy.errors import IndyError
 LOGGER = logging.getLogger(__name__)
 
 
-async def verify_headers(headers, key_finder: KeyFinderBase, method=None, path=None):
+async def verify_signature(
+        headers: Mapping, key_finder: KeyFinderBase, method: str = None, path: str = None):
+    """
+    Verify the DID signature on request headers
+
+    Args:
+        headers: the collection of request headers
+        key_finder: the key finder (likely IndyKeyFinder or KeyCache)
+        method: the HTTP method
+        path: the request path including the query string
+    """
     verifier = HeaderVerifier(key_finder)
     try:
         return await verifier.verify(headers, method, path)
@@ -47,16 +62,24 @@ class IndyKeyFinder(KeyFinderBase):
         self._verifier_id = verifier_id
 
     async def lookup_key(self, key_id: str, key_type: str):
-        assert key_type == "ed25519"
+        if key_type != "ed25519":
+            return None
         if key_id.startswith("did:sov:"):
             short_key_id = key_id[8:]
         else:
             short_key_id = key_id
             key_id = "did:sov:" + short_key_id
 
-        LOGGER.debug(
-            "Fetching verkey for DID '{}' from ledger".format(key_id))
+        LOGGER.debug("Fetching verkey for DID '%s' from ledger", key_id)
         nym_info = await self._client.resolve_nym(short_key_id, self._verifier_id)
         if nym_info.data:
             return decode_string(nym_info.data["verkey"], "base58")
         return None
+
+
+class KeyCache(StaticKeyFinder):
+    def __init__(self, source: KeyFinderBase):
+        super(KeyCache, self).__init__(source, True)
+
+
+__all__ = ('IndyKeyFinder', 'KeyCache', 'KeyFinderBase', 'verify_signature')
