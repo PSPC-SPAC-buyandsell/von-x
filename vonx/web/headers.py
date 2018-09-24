7b90e007cf50a083c77d5a22d5c351840a8797ec
Andrew Whitehead <andrew@1crm.com>
2018-09-24 15:33:20 -0700

    expand abbreviated verkey

--- vonx/web/headers.py
+++ vonx/web/headers.py
@@ -74,13 +74,14 @@ class IndyKeyFinder(KeyFinderBase):
         LOGGER.debug("Fetching verkey for DID '%s' from ledger", key_id)
         nym_info = await self._client.resolve_nym(short_key_id, self._verifier_id)
         if nym_info.data and nym_info.data.get("verkey"):
-            try:
+            if verkey[0] == "~":
+                did = decode_string(nym_info.data["dest"], "base58")
+                suffix = decode_string(verkey[1:], "base58")
+                return did + suffix
+            else:
                 return decode_string(nym_info.data["verkey"], "base58")
             except ValueError:
-                try:
-                    return decode_string(nym_info.data["verkey"], "base64")
-                except ValueError:
-                    raise IndyError("Cannot decode verkey from ledger as base58 or base64: {}".format(nym_info.data["verkey"]))
+                raise IndyError("Cannot decode verkey from ledger as base58: {}".format(nym_info.data["verkey"]))
         return None
 
 
