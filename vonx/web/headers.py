4ba2d0601f0f84d335ec43f35823f2a344cdf8fe
Ian Costanzo <ian@anon-solutions.ca>
2018-09-24 10:21:01 -0700

    If verkey starts with ~ concat did + verkey

--- vonx/web/headers.py
+++ vonx/web/headers.py
@@ -75,10 +75,14 @@ class IndyKeyFinder(KeyFinderBase):
         nym_info = await self._client.resolve_nym(short_key_id, self._verifier_id)
         if nym_info.data and nym_info.data.get("verkey"):
             try:
-                return decode_string(nym_info.data["verkey"], "base58")
+                verkey = nym_info.data["verkey"]
+                if verkey[0] == '~':
+                    did = nym_info.data["dest"]
+                    verkey = did + verkey[1:]
+                return decode_string(verkey, "base58")
             except ValueError:
                 try:
-                    return decode_string(nym_info.data["verkey"], "base64")
+                    return decode_string(verkey, "base64")
                 except ValueError:
                     raise IndyError("Cannot decode verkey from ledger as base58 or base64: {}".format(nym_info.data["verkey"]))
         return None
