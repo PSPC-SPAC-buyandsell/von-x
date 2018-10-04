26be53fbd8a299dd8c027e749453a84eec851fa4
Andrew Whitehead <andrew@1crm.com>
2018-10-04 13:06:07 -0700

    update version to 1.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 pkg_name = 'vonx'
-version = '1.3.6'
+version = '1.4.0'
 
 setup(
     name=pkg_name,
--- vonx/__init__.py
+++ vonx/__init__.py
@@ -19,4 +19,4 @@
 vonx module initialization
 """
 
-__version__ = '1.3.6'
+__version__ = '1.4.0'
