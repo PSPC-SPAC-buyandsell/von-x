4186057598d9b48602fc59f6d92c27ba41aba52e
Andrew Whitehead <andrew@1crm.com>
2018-11-19 12:05:18 -0800

    update dependencies

--- setup.py
+++ setup.py
@@ -32,8 +32,9 @@ setup(
     install_requires=[
         'aiohttp~=3.3.0',
         'aiohttp-jinja2~=1.1.0',
+        'didauth==1.2.3',
         'PyYAML',
         'networkx>=2.2,<3'
-        'von-anchor==1.6.36',
+        'von-anchor==1.6.37',
     ],
 )
